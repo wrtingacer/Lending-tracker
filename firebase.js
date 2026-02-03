823c4a442b183886730090014f70829d9cb42944
writingacer <writingacer@gmail.com>
2026-02-04 00:50:19 +0300

    Remove script tag comment from firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 // ===== firebase.js - Firebase, Auth, Currency, Sharing, Backup =====
 // NOTE: Use <script type="module"> for this file in index.html:
 // <script type="module" src="firebase.js"></script>
- <script type="module" src="firebase.js">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import {
     getAuth, onAuthStateChanged,
